8b5a36f11803e5aaf001d3203da8d482ff12276c
DESKTOP-BE2SPQH\duxin <duxin_be@outlook.com>
2018-07-12 23:31:41 +0900

    no message

--- BloombergMarketsNews/settings.py
+++ BloombergMarketsNews/settings.py
@@ -9,6 +9,9 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
+
+import random
+
 BOT_NAME = 'BloombergMarketsNews'
 
 SPIDER_MODULES = ['BloombergMarketsNews.spiders']
@@ -17,9 +20,14 @@ NEWSPIDER_MODULE = 'BloombergMarketsNews.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'BloombergMarketsNews (+http://www.yourdomain.com)'
+UserAgentList = [
+     "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+     "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+     ]
+USER_AGENT = random.choice(UserAgentList)
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,13 +35,13 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
--- BloombergMarketsNews/spiders/newslist.py
+++ BloombergMarketsNews/spiders/newslist.py
@@ -6,20 +6,69 @@ email     : duxn_be@outlook.com
 """
 
 import scrapy
+from xml.etree.ElementTree import fromstring
+#import logging
+
+from scrapy.spiders import SitemapSpider
+class MySpider(SitemapSpider):
+    #self.log("log1")
+    name='myspider'
+    #sitemap_urls = ['file:///C://Users/duxin/Codes/source/BloombergMarketsNews/BloombergMarketsNews/data/sitemap.xml']
+    sitemap_urls = ['https://www.bloomberg.com/feeds/markets/sitemap_index.xml']
+    sitemap_rules = [
+        ('/articles/', 'parse2')
+    ]
+    sitemap_follow = ['/sitemap_recent']
+
+    def parse1(self, response):
+        print('a:','absdfasdfasdfasdfasdfsd')
+        print('\n\n\n\n')
+    
+    def parse2(self, response):
+        """
+        responseURL = response.url
+        requestURL = response.request.meta['redirect_urls']
+        print('''Response's URL: ''', response.url)
+        print('''Request's  URL: ''', response.meta['url'])
+        if str(responseURL).__eq__(requestURL):
+            pass
+        else:
+            print('--------------------->>>>>>>>Your request is redirect,retrying.....<<<<<-------------------------')
+            yield scrapy.Request(url=requestURL, headers=headers, meta={'url': requestURL}, callback=self.parse)"""
+        print('b:',len(response.text))
+
 
 class GetNewslist(scrapy.Spider):
-    name = "spyder_newslist"
+    name = "spider_newslist"
 
     def start_requests(self):
         urls = [
             "https://www.bloomberg.com/feeds/markets/sitemap_index.xml",
         ]
+        self.article_lists = []
+        self.articles = set()
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            print('Hello World!\n\n\n\n\n\n\n')
+            yield scrapy.Request(url=url, callback=self.parse_sitemap)
+        print('aaaaaaaaaaaaaaaaaaaaaaaaaa\n\n\n\n\n\n\n')
+        for i,url in enumerate(self.article_lists):
+            print('progress: %d / %d'%(i, len(self.article_lists)))
+            yield scrapy.Request(url=url, callback=self.parse_article_lists)
+        self.articles = sorted(list(self.articles), key = lambda x:x[1])
+        print(self.articles[-100:])
+        print(len(self.articles))
 
-    def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+    def parse_sitemap(self, response):
+        print(response.status)
+        print(len(response.text))
+        et = fromstring(response.text)
+        for node in et:
+            print(node[0].text)
+            self.article_lists.append(node[0].text)
+        print('Article lists reading finished!')
+    
+    def parse_article_lists(self, response):
+        et = fromstring(response.text)
+        for node in et:
+            self.articles.add((None, node[1].text.replace('T',' ').replace('Z',''),
+                               node[0].text, False))
